de535f29cfccde6214c58c93064abdcc8b41f780
unknown <kauagg111@gmail.com>
2022-12-09 22:06:14 +0530

    update api to read from db

--- app.js
+++ app.js
@@ -41,9 +41,22 @@ const preferenceSchema = new mongoose.Schema({
 const userDataSchema = new mongoose.Schema({
   email: String,
   likes: Array,
-  bookmark: Array
-})
+  dislikes: Array,
+  bookmark: Array,
+});
+const newsSchema = new mongoose.Schema({
+  author: String,
+  title: String,
+  description: String,
+  url: String,
+  urlToImage: String,
+  publishedAt: String,
+  category: String,
+  likes: Number,
+  dislikes: Number,
+});
 
+const News = new mongoose.model("News", newsSchema);
 const User = new mongoose.model("User", userSchema);
 const Preference = new mongoose.model("Preference", preferenceSchema);
 const UserData = new mongoose.model("UserData", userDataSchema);
@@ -148,47 +161,195 @@ app.post("/getFullNews", (req, res) => {
     });
     let article = new Readability(dom.window.document).parse();
     // res.send(article.content);
-    console.log(article.textContent);
+    // console.log(article.textContent);
     res.send(article.textContent);
   });
 });
 
 app.post("/getAllNews", (req, res) => {
-  axios
-    .get(
-      "https://newsapi.org/v2/top-headlines?country=in&apiKey=" +
-        process.env.NEWS_API
-    )
-    .then((response) => {
-      console.log("data fetched successfully");
-      res.send(response.data);
-    })
-    .catch((err) => {
-      console.log(err.response.status);
-      res.send(err.response.status);
-    });
+  // axios
+  //   .get(
+  //     "https://newsapi.org/v2/top-headlines?country=in&apiKey=" +
+  //       process.env.NEWS_API
+  //   )
+  //   .then((response) => {
+  //     console.log("data fetched successfully");
+  //     res.send(response.data);
+  //   })
+  //   .catch((err) => {
+  //     console.log(err.response.status);
+  //     res.send(err.response.status);
+  //   });
+  console.log("get news requested");
+  News.find({}, null, { limit: 1000 }, function (err, docs) {
+    if (err) {
+      // res.status(502);
+      res.send("error");
+    } else {
+      // console.log(docs);
+      if (docs && docs.length !== 0) {
+        // res.status(200);
+        res.send(docs);
+      } else {
+        // res.status(404);
+        res.send("no news found");
+      }
+    }
+  });
 });
 
-app.post("/addBookmark", (req, res)=>{
+app.post("/addBookmark", (req, res) => {
   const data = req.body;
   console.log(data);
-  UserData.findOneAndUpdate({email: data.email}, {$push: {bookmark: data.bookmark}}, {upsert: true}, function(err){
-    if(!err){
-      res.send("bookmark added");
-    }else{
-      res.send("err");
-    }
-  })
+  UserData.updateOne(
+    { email: data.email },
+    { $addToSet: { bookmark: data.bookmark } },
+    { upsert: true },
+    function (err) {
+      if (!err) {
+        console.log("book mark added");
+        res.send("bookmark added");
+      } else {
 
+        res.send("err");
+      }
+    }
+  );
 });
 
-app.post("/fetchBookmark", (req, res)=>{
+app.post("/fetchBookmark", (req, res) => {
   const data = req.body;
-  UserData.findOne({email: data.email}, function(err, data){
-    if(!err){
-      res.send(data);
+  UserData.findOne({ email: data.email }, function (err, data) {
+    if (!err) {
+      if(data && data.bookmark){
+        res.send(data.bookmark);
+      }else{
+        res.send([]);
+      }
+    } else {
+      res.send("error in fetching bookmark");
+    }
+  });
+});
+
+app.post("/like", (req, res) => {
+  const data = req.body;
+  const email = data.email;
+  const _id = data._id;
+  let prevLike = 0;
+  News.findOne({ _id: _id }, null, function (err, docs) {
+    if (err) {
+      console.log("error");
+      res.send("error");
+    } else {
+      if (docs && docs.length !== 0) {
+        console.log(docs);
+        prevLike = docs.likes;
+        console.log(prevLike);
+        News.findOneAndUpdate(
+          { _id: _id },
+          { likes: prevLike + 1 },
+          { upsert: true },
+          function (err) {
+            if (err) {
+              console.log(err);
+              res.send(err);
+            } else {
+              console.log("article liked");
+            }
+          }
+        );
+      }
+    }
+  });
+  
+  UserData.findOneAndUpdate(
+    { email: email },
+    { $addToSet: { likes: _id } },
+    { upsert: true },
+    function (err) {
+      if (!err) {
+        res.send("like added");
+      } else {
+        res.send("err");
+      }
+    }
+  );
+});
+
+app.post("/dislike", (req, res) => {
+  const data = req.body;
+  const email = data.email;
+  const _id = data._id;
+  let prevdislike = 0;
+  News.findOne({ _id: _id }, null, function (err, docs) {
+    if (err) {
+      console.log("error");
+      res.send("error");
+    } else {
+      if (docs && docs.length !== 0) {
+        console.log(docs);
+        prevdislike = docs.dislikes;
+        console.log(prevdislike);
+        News.findOneAndUpdate(
+          { _id: _id },
+          { dislikes: prevdislike + 1 },
+          { upsert: true },
+          function (err) {
+            if (err) {
+              console.log(err);
+              res.send(err);
+            } else {
+              console.log("article disliked");
+            }
+          }
+        );
+      }
+    }
+  });
+  
+  UserData.findOneAndUpdate(
+    { email: email },
+    { $addToSet: { dislikes: _id } },
+    { upsert: true },
+    function (err) {
+      if (!err) {
+        res.send("dislike added");
+      } else {
+        res.send("err");
+      }
+    }
+  );
+});
+
+app.post("/getNewsById", (req, res)=>{
+  const _id = req.body._id;
+  console.log(_id);
+  News.findOne({_id: _id}, null, function(err, docs){
+    if(err){
+      res.send('error');
+      console.log('error in fetching news by id');
     }else{
-      res.send("error in fetching bookmark")
+      if(docs){
+        console.log("news fetched by id");
+        res.send(docs)
+      }else{
+        console.log("no news by id found");
+        res.send("error");
+      }
+    }
+  })
+})
+
+app.post("/search", (req, res)=>{
+  const text = req.body.text;
+  News.find({title: {"$regex": text, "$options": "i"}}, function(err, docs){
+    if(err){
+      console.log("error while searching");
+      res.send(err);
+    }else{
+      console.log(docs);
+      res.send(docs);
     }
   })
 })
@@ -217,6 +378,6 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = 8000;
 }
-app.listen(port, function(){
+app.listen(port, function () {
   console.log("server has started at port " + port);
 });
